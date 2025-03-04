f538f438a13c946ad3473bffa5a9e143203177b7
Anita Ramirez <eliayala1105@gmail.com>
2025-03-03 21:29:05 -0300

    agregar amigos

--- app/src/main/java/com/ugb/miprimeraaplicacion/DB.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/DB.java
@@ -12,6 +12,7 @@ public class DB extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQLdb);
